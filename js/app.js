8dcc4b556d6d6ee3aa6616b8c120e9752735236b
towoloju <oluwatowoakinbode@gmail.com>
2021-04-26 19:01:56 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+//Select tabs on click
+// $(".nav li").on("click", function(){
+//     $(".nav li").removeClass("active");
+//     $(this).addClass("active");
+// });
+
+
+//select table by ID/href
+// $(".nav li").on("click", function(){
+//     $('#myTabs a[href="#profile"]').tab('show') // Select tab by name
+// });
+
